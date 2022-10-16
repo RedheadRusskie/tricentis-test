23debf54d0c25c3e365b8ee1a0a1a49465f6b551
Ivan <88277309+RedheadRusskie@users.noreply.github.com>
2022-10-16 21:55:26 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import "./App.css";
-
-// components
 import { NavBar } from "./components/NavBar/NavBar";
 import { Main } from "./pages/Main/Main";
 import { TaskII } from "./pages/TaskII/TaskII";
-
-// hooks
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
